642168bfffbd27cca94b7b2882ab8a18de062c52
seifdenguezli <seifeddine.denguezli@esprit.tn>
2021-11-17 23:52:20 +0100

    Integration Backoffice

--- SymfonyProject/config/bundles.php
+++ SymfonyProject/config/bundles.php
@@ -17,4 +17,5 @@ return [
 
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Craue\FormFlowBundle\CraueFormFlowBundle::class => ['all' => true],
 ];
