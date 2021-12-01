540deb7c1737dfd5b75a5d971b5ea6bd54d1c9a4
aymenzaabar <aymen.zaabar@esprit.tn>
2021-12-01 03:05:39 +0100

    forgot pass2

--- SymfonyProject/src/Controller/UserController.php
+++ SymfonyProject/src/Controller/UserController.php
@@ -11,6 +11,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Faker\Factory;
 
+use Knp\Component\Pager\PaginatorInterface; // Nous appelons le bundle KNP Paginator
+
+
 
 /**
  * @Route("/user")
@@ -20,11 +23,16 @@ class UserController extends AbstractController
     /**
      * @Route("/", name="user_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator): Response
     {
-        $users = $this->getDoctrine()
+        $donnees = $this->getDoctrine()
             ->getRepository(User::class)
             ->findAll();
+        $users = $paginator->paginate(
+            $donnees, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
 
         return $this->render('user/index.html.twig', [
             'users' => $users,
